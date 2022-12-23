b57f928a258ea79c4f4c2c5e857e870ad5ab4ef1
nurget <101033246+nurget@users.noreply.github.com>
2022-12-23 23:04:16 +0900

    Initial Commit

---
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIMemoApp.swift
+//  SwiftUIMemo
+//
+//  Created by 김아영 on 2022/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
