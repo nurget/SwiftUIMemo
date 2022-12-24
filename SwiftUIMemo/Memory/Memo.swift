c87167cec92946015ab6d7db34501159137ee372
nurget <101033246+nurget@users.noreply.github.com>
2022-12-24 14:02:51 +0900

    memo list screen view

--- SwiftUIMemo/Memory/Memo.swift
+++ SwiftUIMemo/Memory/Memo.swift
@@ -11,7 +11,7 @@ import SwiftUI
 // Identifiable은 데이터를 리스트에 쉽게 바인딩
 // ObservedObject는 메모를 편집할 때, 뷰를 자동으로 업데이트 할 때
 
-class Memo: Identifiable, ObservedObject {
+class Memo: Identifiable, ObservableObject {
     let id: UUID
     @Published var content: String // Published 특성은 새로운 값을 저장할 때마다 바인딩 되어 있는 UI가 자동으로 업데이트 됨.
     let insertDate: Date // 날짜를 저장하는 속성
--- SwiftUIMemo/SwiftUIMemoApp.swift
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoApp: App {
+    @StateObject var store = MemoStore() // 메모 저장소를 속성으로 저장
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store) // mainListView, 이어지는 뷰에서 동일한 객체를 쉽게 사용 가능
         }
     }
 }
